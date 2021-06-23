490ef207a5014ce7e2328332a0886bb17aede6a4
Arif Uddin <arifunctg@gmail.com>
2021-06-23 16:39:07 +0600

    cleaned

--- src/components/Image.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default function Image({ src, w, h, alt }) { return <img className="doc-img" loading="lazy" src={`/img/${src}.png`} width={w} height={h} alt={alt} /> }
--- src/components/Video.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function Video({ src, w, h, alt }) {
-    return (
-        <video width={w} height={h} controls>
-            <source src={`/video/${src}.mp4`} type="video/mp4" />
-        </video>
-    )
-}
