a897941c73109327ff88168c51b58a373a46ac56
Arif Uddin <arifunctg@gmail.com>
2022-03-05 16:30:56 +0600

    pkg version upgraded

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,23 +13,6 @@ module.exports = {
       defaultMode: 'light',
       disableSwitch: false,
       respectPrefersColorScheme: true,
-      switchConfig: {
-        darkIcon: '💡',
-        // CSS to apply to dark icon,
-        // React inline style object
-        // see https://reactjs.org/docs/dom-elements.html#style
-        darkIconStyle: {
-          marginLeft: '2px',
-        },
-
-        // Unicode icons such as '\u2600' will work
-        // Unicode with 5 chars require brackets: '\u{1F602}'
-        lightIcon: '🌑',
-
-        lightIconStyle: {
-          marginLeft: '1px',
-        },
-      },
     },
     navbar: {
       title: 'Bit Form',
